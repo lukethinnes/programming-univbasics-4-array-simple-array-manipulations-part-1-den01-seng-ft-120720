0a149eabd4bb04e2f50f344adcc591abebc9603f
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 06:08:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(aray, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop()
+end
+
+def pop_with_args(array)
+
+end
+
+def using_shift(array)
+
+end
+
+def shift_with_args(array)
+
+end
